c04d092ae3aea4553b0b426736cf30cd321eb313
Bipro <biprobarai18@gmail.com>
2022-07-12 21:00:33 +0600

    courses

--- src/pages/Home/Courses/Courses.js
+++ src/pages/Home/Courses/Courses.js
@@ -1,61 +1,42 @@
-import React from "react";
+import { Container } from "@mui/material";
+import React, { useState } from "react";
 import { useEffect } from "react";
-import { useState } from "react";
-import ReactPaginate from "react-paginate";
-import Course from "../Course/Course";
+import { Col, Row, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Rating from "react-rating";
 import "./Courses.css";
 const Courses = () => {
-  const [courses, setCourses] = useState([]);
-  const [currentItems, setCurrentItems] = useState(0);
-  const [pageCount, setPageCount] = useState(0);
-  const [itemOffset, setItemOffset] = useState(0);
-
+  const [courses, setCourse] = useState([]);
+  // fake data is loading from json
   useEffect(() => {
     fetch("./courses.json")
       .then((res) => res.json())
-      .then((data) => setCourses(data));
+      .then((data) => setCourse(data));
   }, []);
-
-  const itemsPerPage = 1;
-
-  useEffect(() => {
-    const endOffset = itemOffset + itemsPerPage;
-    setCurrentItems(courses.slice(itemOffset, endOffset));
-    setPageCount(Math.ceil(courses.length / itemsPerPage));
-  }, [itemOffset, itemsPerPage, courses]);
-
-  // Invoke when user click to request another page.
-  const handlePageClick = (event) => {
-    const newOffset = (event.selected * itemsPerPage) % courses.length;
-    setItemOffset(newOffset);
-  };
-
   return (
-    <>
-      <div className="headline">
-        <h3>Enroll a course hurry up!!</h3>
-      </div>
-      <div>
-        {courses.map((course) => (
-          <Course course={course} key={course.id} />
-        ))}
-        {/* <Items currentItems={currentItems} /> */}
-      </div>
-      <ReactPaginate
-        breakLabel="..."
-        nextLabel="Next >"
-        onPageChange={handlePageClick}
-        pageRangeDisplayed={1}
-        pageCount={pageCount}
-        previousLabel="< Previous"
-        renderOnZeroPageCount={null}
-        containerClassName="pagination"
-        pageLinkClassName="page-num"
-        previousLinkClassName="page-num"
-        nextLinkClassName="page-num"
-        activeClassName="active"
-      />
-    </>
+    <div>
+      <Container className="pb-4">
+        <Row sm={1} md={2} lg={3} className="  g-3">
+          {courses.map((course) => (
+            <Container className=" ">
+              <div className="photo-frame">
+                <div className="photo">
+                  <img
+                    src={course.img}
+                    style={{ width: "300px", height: "200px" }}
+                    alt=""
+                  />
+                </div>
+                <div className=" photo-detail">
+                  <h3>Course:{course.name}</h3>
+                  <h3>Category:{course.category}</h3>
+                </div>
+              </div>
+            </Container>
+          ))}
+        </Row>
+      </Container>
+    </div>
   );
 };
 
--- src/pages/Home/Review/Review.js
+++ src/pages/Home/Review/Review.js
@@ -1,15 +1,72 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+import data from "./data";
 const Review = () => {
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, people]);
+
+  // autoslide, clearInterval = een cleanup functie noodzakelijk bij interval
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 3000);
+    return () => clearInterval(slider);
+  }, [index]);
+
   return (
-    <div>
-      <h2>Client say</h2>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis
-        perspiciatis unde blanditiis modi esse, nostrum culpa aut, molestiae
-        facere praesentium ducimus beatae, repellendus at animi vel libero
-        mollitia et adipisci.
-      </p>
+    <div className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>reviews
+        </h2>
+      </div>
+      <div className="section-center">
+        {people.map((person, personIndex) => {
+          const { id, image, name, title, quote } = person;
+          let position = "nextSlide";
+          if (personIndex === index) {
+            position = "activeSlide";
+          }
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)
+          ) {
+            position = "lastSlide";
+          }
+          return (
+            <div key={id} className={position}>
+              <img
+                src={image}
+                alt={name}
+                className="person-img"
+                style={{ width: "300px" }}
+                v
+              />
+              <h4>{name}</h4>
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className="icon" />
+            </div>
+          );
+        })}
+        <button className="prev" onClick={() => setIndex(index - 1)}>
+          <FiChevronLeft />
+        </button>
+        <button className="next" onClick={() => setIndex(index + 1)}>
+          <FiChevronRight />
+        </button>
+      </div>
     </div>
   );
 };
